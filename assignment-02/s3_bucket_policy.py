c0e918f20664198a28e5e2f44b56cea157ab086c
Nika <freexnick@yahoo.com>
2022-04-06 00:55:45 +0400

    task02:s3_bucket_policy

---
+++ assignment-02/s3_bucket_policy.py
@@ -0,0 +1,43 @@
+from email.policy import Policy
+import json
+import boto3
+from pprint import pprint
+from botocore.exceptions import ClientError
+
+s3 = boto3.client('s3')
+
+
+def check_policy(bucket_name):
+    try:
+        response = s3.get_bucket_policy(Bucket=bucket_name)
+        pprint(response.get('Policy'))
+    except ClientError as e:
+        return apply_policy(bucket_name)
+
+
+def generate_public_read_policy(bucket_name):
+    return json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                'Resource': [
+                    f'arn:aws:s3:::{bucket_name}/dev/*',
+                    f'arn:aws:s3:::{bucket_name}/test/*',
+                ],
+            }
+        ]
+    })
+
+
+def apply_policy(bucket_name):
+    if s3.put_bucket_policy(Bucket=bucket_name,
+                            Policy=generate_public_read_policy(bucket_name)):
+        print(f'policy has been applied to {bucket_name}')
+
+
+if __name__ == '__main__':
+    check_policy('btu-devops-nk-2022222')

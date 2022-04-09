6c861af6c3700658234c6665a9736be89169580d
Nika <freexnick@yahoo.com>
2022-04-09 13:51:44 +0400

    task01:s3_list:refactor

--- assignment-01/s3_list.py
+++ assignment-01/s3_list.py
@@ -2,19 +2,22 @@ import boto3
 
 s3 = boto3.client('s3')
 
-def get_buckets(bucketName = ""):
+
+def get_buckets(bucket_name=""):
     response = s3.list_buckets()['Buckets']
-    if bucketName:
-        match = [s for s in response if s['Name'].__contains__(bucketName.lower())]
+    if bucket_name:
+        match = [s for s in response if s['Name'].__contains__(
+            bucket_name.lower())]
         if match:
             print("found matching buckets:")
             for bucket in match:
                 print(f'{bucket["Name"]}')
         else:
-            print("no bucket with such a name!")   
+            print("no bucket with such a name!")
     else:
         for bucket in response:
             print(f'{bucket["Name"]}')
+
 
 if __name__ == '__main__':
     get_buckets("prod")

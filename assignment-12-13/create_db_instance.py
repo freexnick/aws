5a537f32dd3a7861c46a6cc31ee9e49004c39c3e
Nika <freexnick@yahoo.com>
2022-05-30 23:48:43 +0400

    task12-13:crete_db_instance_refactor

--- assignment-12-13/create_db_instance.py
+++ assignment-12-13/create_db_instance.py
@@ -4,20 +4,6 @@ from botocore.exceptions import ClientError
 rds = boto3.client('rds')
 
 
-def determine_port(db_engine):
-    match db_engine.lower():
-        case "aurora" | "mysql" | "mariadb":
-            return 3306
-        case "postgre" | "postgresql":
-            return 5432
-        case "oracle":
-            return 1521
-        case "sql" | "sql server":
-            return 1433
-        case default:
-            return
-
-
 def create_db_instance(name, engine, storage='60', username='admin', password='admin'):
     try:
         rds.create_db_instance(
@@ -29,7 +15,6 @@ def create_db_instance(name, engine, storage='60', username='admin', password='a
             MasterUsername=username,
             MasterUserPassword=password,
             BackupRetentionPeriod=7,
-            Port=determine_port(engine),
             MultiAZ=False,
             AutoMinorVersionUpgrade=True,
             PubliclyAccessible=True,
